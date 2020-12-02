27e24e039ab982f615c1c2793683e52743f43f0b
ronaldo.caetano <ronaldo.caetano@acct.global>
2020-12-02 09:39:17 -0300

    adding graphql config

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,11 +1,13 @@
 const postcss = require('rollup-plugin-postcss')
+const graphql = require('rollup-plugin-graphql')
 
 module.exports = {
   rollup(config) {
     config.plugins.push(
       postcss({
         modules: true,
-      })
+      }),
+      graphql()
     )
 
     return config
